b3540ce746f1bcdb105969f13fb96a1be54e5895
Zikam1 <isaac.aku@cyphercrescent.com>
2024-11-13 06:31:14 +0100

    feat:adde the PNG file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.js
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  vite: {
+    assetsInclude: ['**/*.PNG']  // Add this line to include PNG files as assets
+  }
 })
